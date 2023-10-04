a3480616e119e8392c74ce080e55e2a03c3d59ca
YY Ptushko <111455738+ultyyp@users.noreply.github.com>
2023-10-04 16:08:31 +0300

    Added Tests

--- tests/OnlineShop.Domain.Test/CartTests.cs
+++ tests/OnlineShop.Domain.Test/CartTests.cs
@@ -9,7 +9,7 @@ namespace MyOnlineShop.Domain.Test
         {
             //Arrange
             var cart = new Cart(Guid.NewGuid(), Guid.NewGuid());    
-            var productId = Guid.NewGuid(); //a
+            var productId = Guid.NewGuid();
 
             //Act
             cart.AddItem(productId, 1);
